9fd218fe0ecbbe172f6b7213a034e367495ff928
madascicom <137607688+madascicom@users.noreply.github.com>
2023-06-24 20:25:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,17 +10,14 @@ def generate_response(uploaded_file, openai_api_key, query_text):
     if uploaded_file is not None:
         documents = [uploaded_file.read().decode()]
         # Split documents into chunks
-        text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=10)
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
         texts = text_splitter.create_documents(documents)
         # Select embeddings
         embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
         # Create a vectorstore from documents
-        db = Chroma.from_documents(texts, embeddings)
-        # Create retriever interface
-        retriever = db.as_retriever()
-        # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='map_reduce', retriever=retriever)
-        return qa.run(query_text)
+      docsearch = Chroma.from_documents(texts, embeddings)
+qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever())
+return qa.run(query_text)
 
 # Page title
 st.set_page_config(page_title='🦜🔗 Mada 3 Ask the Doc App')
